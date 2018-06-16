096a92b759f9ef49ed9264bc8836618674c918aa
Marcos <mcsbrigida@hotmail.com>
2018-06-16 18:46:38 -0300

    Nova Branch

---
+++ controller/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+class Fazenda {
+
+    private $idFazenda;
+    private $nome;
+
+    function getIdFazenda() {
+        return $this->idFazenda;
+    }
+
+    function getNome() {
+        return $this->nome;
+    }
+
+    function setIdFazenda($idFazenda) {
+        $this->idFazenda = $idFazenda;
+    }
+
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+}
